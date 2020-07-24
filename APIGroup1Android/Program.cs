b496146519216fc65e9d20cfa98d87b60462dd7b
Haoli Lu <169606@nzse.ac.nz>
2020-07-24 12:12:07 +1200

    here is my change.

--- APIGroup1Android/Program.cs
+++ APIGroup1Android/Program.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -14,7 +14,7 @@ namespace APIGroup1Android
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            //jnhkuhig
+            //API change
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
