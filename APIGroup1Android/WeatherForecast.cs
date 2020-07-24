d404bfc59b5b75f43609ce07b7dd4cc44deff58e
Mingqian Xie <i157859@nzse.ac.nz>
2020-07-24 12:14:57 +1200

    testing MIngqian

--- APIGroup1Android/WeatherForecast.cs
+++ APIGroup1Android/WeatherForecast.cs
@@ -4,9 +4,9 @@ namespace APIGroup1Android
 {
     public class WeatherForecast
     {
-        //TestMing
+        
         public DateTime Date { get; set; }
-        //testing Ming
+      
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
