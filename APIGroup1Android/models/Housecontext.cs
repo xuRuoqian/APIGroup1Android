b6c866ca0189545b815df154ee43dfd7913f6779
Mingqian Xie <i157859@nzse.ac.nz>
2020-08-09 17:38:19 +1200

    new change 09/08/2020

---
+++ APIGroup1Android/models/Housecontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGroup1Android.models
+{
+    public class Housecontext : DbContext
+    {
+
+        public Housecontext(DbContextOptions<Housecontext> op) : base(op)
+        { }
+
+        public DbSet<House> houses;
+
+        public DbSet<APIGroup1Android.models.House> Houses { get; set; }
+
+    }
+}
