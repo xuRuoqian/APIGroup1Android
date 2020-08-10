aa1f0178b0d98c1d0f23e1039aa26fb034a5e41f
Ruoqian XU <1812924@nzse.ac.nz>
2020-08-11 10:22:25 +1200

    Changing StartUp after merge

--- APIGroup1Android/Startup.cs
+++ APIGroup1Android/Startup.cs
@@ -27,6 +27,8 @@ namespace APIGroup1Android
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Housecontext>(opt => opt.UseInMemoryDatabase("HouseList"));
+            services.AddDbContext<usercontext>(opt => opt.UseInMemoryDatabase("UserList"));
             services.AddControllers();
         }
 
